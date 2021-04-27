723530196b9aa3f5acbf92d0940906d102c95fc0
Unruh-Spirale <protjan@wp.pl>
2021-04-27 16:39:34 +0200

    Create new project

---
+++ src/main/java/com/simulation/simulationapp/SimulationappApplication.java
@@ -0,0 +1,13 @@
+package com.simulation.simulationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulationappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simulation/simulationapp/SimulationappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulation.simulationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
