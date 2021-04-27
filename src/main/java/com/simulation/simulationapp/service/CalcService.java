00c7ce2b21a5ad11480c1ff75a25358121613c54
Unruh-Spirale <protjan@wp.pl>
2021-04-27 23:06:02 +0200

    Create CalcService interface
    This interface contains method to calculate simulation

---
+++ src/main/java/com/simulation/simulationapp/service/CalcService.java
@@ -0,0 +1,8 @@
+package com.simulation.simulationapp.service;
+
+import com.simulation.simulationapp.model.Simulation;
+
+public interface CalcService {
+
+    void calcSimulation(Simulation simulation);
+}
