43e2b678a08ff6e692822813c5ed3d6ebed367e9
Unruh-Spirale <protjan@wp.pl>
2021-04-28 21:01:32 +0200

    Add first simulation data to app

--- src/main/java/com/simulation/simulationapp/SimulationappApplication.java
+++ src/main/java/com/simulation/simulationapp/SimulationappApplication.java
@@ -1,13 +1,23 @@
 package com.simulation.simulationapp;
 
+import com.simulation.simulationapp.model.Simulation;
+import com.simulation.simulationapp.service.SimulationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SimulationappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SimulationappApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(SimulationappApplication.class, args);
+
+        Simulation sim1 = new Simulation("First Simulation", 20000,1,2,0.1,4,2,20);
+
+        SimulationService simulationService = ctx.getBean(SimulationService.class);
+
+        simulationService.addSimulation(sim1);
+
     }
 
 }
