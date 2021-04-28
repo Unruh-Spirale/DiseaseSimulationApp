91a1632049975db3875ae218e596fb62676bca2a
Unruh-Spirale <protjan@wp.pl>
2021-04-28 22:51:21 +0200

    Add two additional simulation in SimulationappApplication class

--- src/main/java/com/simulation/simulationapp/SimulationappApplication.java
+++ src/main/java/com/simulation/simulationapp/SimulationappApplication.java
@@ -13,10 +13,14 @@ public class SimulationappApplication {
         ConfigurableApplicationContext ctx = SpringApplication.run(SimulationappApplication.class, args);
 
         Simulation sim1 = new Simulation("First Simulation", 20000,1,2,0.1,4,2,20);
+        Simulation sim2 = new Simulation("Second Simulation", 5000,1,1.5,0.2,10,5,25);
+        Simulation sim3 = new Simulation("Third Simulation", 20000,5,3,0.15,4,2,100);
 
         SimulationService simulationService = ctx.getBean(SimulationService.class);
 
         simulationService.addSimulation(sim1);
+        simulationService.addSimulation(sim2);
+        simulationService.addSimulation(sim3);
 
     }
 
